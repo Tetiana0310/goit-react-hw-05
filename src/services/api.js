ab565f7ef9c403859c5f4e4f49bdd90060fde4be
Tetiana Posmashna <tetiana.posmashna@gmail.com>
2024-10-10 19:33:12 +0300

    add axios, add baseURL and apiKey

---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+
+const apiKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjQwNmM2MTc3MWNkYTNhMGFiZTRmZGVlOWZlN2I5NiIsIm5iZiI6MTcyODU3NzUzMi43ODg3MTEsInN1YiI6IjY3MDdmZTQ5NjcxODAxMmZjMjMzZDI5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kym_k_nYC1QkheI4huAL3tCMleG4_BDfKhRBRDzBduY"
